9be1f80e0bbdce15b1bbe40ccc83a07824ffe571
shahzeb7860 <shahzebahmed37@gmail.com>
2024-08-02 00:39:29 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author = 'shahzeb',
+    authoe_email = 'shahzebahmed37@gmail.com',
+    packages= find_packages(),
+    install_requires =['pandas', 'numpy', 'seaborn'] 
+)
+
